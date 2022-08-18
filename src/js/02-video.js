48e77cd718f15eb352ffb44e8101ce2e038e5d7a
whiteroma <roma.white95@gmail.com>
2022-08-18 12:18:21 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,16 @@
 import Player from '@vimeo/player';
-
+import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('timeupdate',  throttle( e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+    }, 1000)
+    );
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    console.error(error)
+});
